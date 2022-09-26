c9469a3afd6468b9fa46586f090bb6c55196d450
Joe P Shoulak <joe@squarecompass.io>
2022-09-26 12:28:04 -0500

    implemented icons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,7 @@
 // Constants
 var testCity = "Minneapolis";
-var endpoint = "https://api.openweathermap.org/data/2.5/";
+var iconURL = "http://openweathermap.org/img/wn/";
+var base = "https://api.openweathermap.org/data/2.5/";
 var apiKey = "fe78b54641e53796124286d599942e78";
 
 // DOM Elements
@@ -8,8 +9,12 @@ var article = $('#five-day-forecast');
 
 // Update the page HTML to show the current weather
 function renderCurrentWeather(data) {
+    console.log(data);
+
     $("#location").text(data.name);
     $("#todays-date").text(moment().format("(MM/DD/YYYY)"));
+
+    $("#todays-icon").attr("src", `${iconURL + data.weather[0].icon}@2x.png`)
 
     $("#todays-temp").text(`${data.main.temp}°F`);
     $("#todays-wind").text(`${data.wind.speed}mph`);
@@ -17,37 +22,41 @@ function renderCurrentWeather(data) {
 }
 
 // Render one of the weather card to be displayed in Forecast
-function renderWeatherCard(weatherData) {
+function renderWeatherCard(data) {
     return $('<div>').addClass('card column col-2')
-      .append($('<h5>').text(weatherData.dt_txt.split(" ")[0]))  // Title / Date
-      .append($('<span>').text(`${weatherData.main.temp}°F`))    // Temp
-      .append($('<span>').text(`${weatherData.wind.speed}mph`))  // Wind
-      .append($('<span>').text(`${weatherData.main.humidity}%`)) // Humidity
+      .append($('<h5>').text(data.dt_txt.split(" ")[0]))  // Title / Date
+      .append($('<img>').attr("src", `${iconURL + data.weather[0].icon}@2x.png`).addClass("icon")) // Icon
+      .append($('<span>').text(`${data.main.temp}°F`))    // Temp
+      .append($('<span>').text(`${data.wind.speed}mph`))  // Wind
+      .append($('<span>').text(`${data.main.humidity}%`)) // Humidity
 }
 
 // Update the page HTML to show the forecasted weather
 function renderForecastedWeather(data) {
-    var forecast = [];
+    var forecast = []; // Make empty array
+    for (i = 0; i < 40; i+=8) { forecast.push(data.list[i]); } // Fill it with every 8th element
 
-    // Grab every 8th time slot
-    for (i = 0; i < 40; i+=8) { forecast.push(data.list[i]); } // THIS IS A LIE
-  
-    article.empty(); // Delete all forecast data
+    article.empty(); // Delete all rendered forecast data
 
     // For every day of forecasted data, append a new forecast card
     forecast.forEach(weatherData => { article.append(renderWeatherCard(weatherData)); });
 }
 
+// Create the proper URL for our API call
+function apiurl(city, forecast=false) {
+    return `${base + (forecast ? "forecast?q=" : "weather?q=") + city}&units=imperial&appid=${apiKey}`
+}
+
 // Get the current weather given a city
 function fetchCurrentWeather(city) {
-    fetch(endpoint + "weather?q=" + city + "&units=imperial&appid=" + apiKey)
+    fetch(apiurl(city))
       .then(response => { return response.json(); })
       .then(data => { renderCurrentWeather(data); });
 }
 
 // Get the current weather given a city
 function fetchForecastWeather(city) {
-    fetch(endpoint + "forecast?q=" + city + "&units=imperial&appid=" + apiKey)
+    fetch(apiurl(city, forecast=true))
       .then(response => { return response.json(); })
       .then(data => { renderForecastedWeather(data); });
 }
