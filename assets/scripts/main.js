14f00016c31c710e0e459cc7dddab53ec6dac862
Joe P Shoulak <joe@squarecompass.io>
2022-10-16 21:51:20 -0500

    polish

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,12 +5,10 @@ const base = "https://api.openweathermap.org/data/2.5/";
 const apiKey = "fe78b54641e53796124286d599942e78";
 
 // DOM Elements
-var article = $('#five-day-forecast');
+const article = $('#five-day-forecast');
 
 // Update the page HTML to show the current weather
-function renderCurrentWeather(data) {
-    console.log(data);
-
+const renderCurrentWeather = (data) => {
     $("#location").text(data.name);
     $("#todays-date").text(moment().format("(MM/DD/YYYY)"));
 
@@ -22,7 +20,7 @@ function renderCurrentWeather(data) {
 }
 
 // Render one of the weather card to be displayed in Forecast
-function renderWeatherCard(data) {
+const renderWeatherCard = (data) => {
     return $('<div>').addClass('card column col-2')
       .append($('<h5>').text(data.dt_txt.split(" ")[0]))  // Title / Date
       .append($('<img>').attr("src", `${iconURL + data.weather[0].icon}@2x.png`).addClass("icon")) // Icon
@@ -32,44 +30,49 @@ function renderWeatherCard(data) {
 }
 
 // Update the page HTML to show the forecasted weather
-function renderForecastedWeather(data) {
-    var forecast = []; // Make empty array
-    for (i = 0; i < 40; i+=8) { forecast.push(data.list[i]); } // Fill it with every 8th element
+const renderForecastedWeather = (data) => {
+    let forecast = []; // Make empty array
+
+    for (i = 0; i < 40; i+=8) {
+        forecast.push(data.list[i]);  // Fill it with every 8th element
+    }
 
     article.empty(); // Delete all rendered forecast data
 
     // For every day of forecasted data, append a new forecast card
-    forecast.forEach(weatherData => { article.append(renderWeatherCard(weatherData)); });
+    forecast.forEach(weatherData => {
+        article.append(renderWeatherCard(weatherData));
+    });
 }
 
 // Create the proper URL for our API call
-function apiurl(city, forecast=false) {
+const apiurl = (city, forecast=false) => {
     return `${base + (forecast ? "forecast?q=" : "weather?q=") + city}&units=imperial&appid=${apiKey}`
 }
 
 // Get the current weather given a city
-function fetchCurrentWeather(city) {
+const fetchCurrentWeather = (city) => {
     fetch(apiurl(city))
       .then(response => { return response.json(); })
       .then(data => { renderCurrentWeather(data); });
 }
 
 // Get the current weather given a city
-function fetchForecastWeather(city) {
+const fetchForecastWeather = (city) => {
     fetch(apiurl(city, forecast=true))
-      .then(response => { return response.json(); })
-      .then(data => { renderForecastedWeather(data); });
+      .then(response =>  response.json() )
+      .then(data => renderForecastedWeather(data) );
 }
 
 // We never update one but not the other, so let's tie these together
-function fetchAllData(city) {
+const fetchAllData = (city) => {
     fetchCurrentWeather(city);
     fetchForecastWeather(city);
 }
 
 // Handle the searching for a new city
-function citySearch() {
-    var city = $("#city-search").val(); // Grab our city
+const citySearch = () => {
+    const city = $("#city-search").val(); // Grab our city
 
     fetchAllData(city); // Fetch and render all the data
 
@@ -77,12 +80,12 @@ function citySearch() {
 }
 
 // Handle clicks on recently searched cities
-function historyClick(event) {
+const historyClick = (event) => {
     fetchAllData($(event.target).text());
 }
 
 // On page first load
-function init() {
+const init = () => {
     fetchAllData(testCity);
 }
 
